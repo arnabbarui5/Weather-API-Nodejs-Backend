d94dbb6c5d065b7d9e86da7bb6ce107552553079
Arnab Barui <baruiarnab51098@gmail.com>
2020-09-30 00:45:40 +0530

    Add files via upload

---
+++ WeatherAppNodejs/weather.js
@@ -0,0 +1,47 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("index.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp - 273.15);
+  temperature = temperature.replace(
+    "{%tempmin%}",
+    orgVal.main.temp_min - 273.15
+  );
+  temperature = temperature.replace(
+    "{%tempmax%}",
+    orgVal.main.temp_max - 273.15
+  );
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "http://api.openweathermap.org/data/2.5/weather?q=Kolkata&appid=4f44375d80ef160ee8a63f1d17b178ec"
+    )
+      .on("data", (chunk) => {
+        const objData = JSON.parse(chunk);
+        const arrData = [objData];
+        // console.log(arrData[0].main.temp - 273.15);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+        // console.log(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to port 8000....");
+});
